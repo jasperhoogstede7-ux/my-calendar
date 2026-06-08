d9e126fdc87147a983c641e2a18160e3c58dd57d
jasperhoogstede7-ux <jasperhoogstede7@gmail.com>
2026-06-09 00:06:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,5 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
-});
-
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
 self.addEventListener('notificationclick', e => {
   e.notification.close();
   e.waitUntil(clients.openWindow('/'));
